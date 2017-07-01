e6d75b74e72429eb1d7c7515955b2366dfa24cdd
xuwanjin <xuwanjin1991@Gmail.com>
2017-07-01 23:36:29 +0800

    add local broadcast
    Signed-off-by: xuwanjin <xuwanjin1991@Gmail.com>

--- BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/BroadcastDemoActivity.java
+++ BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/BroadcastDemoActivity.java
@@ -1,13 +1,17 @@
 package com.example.xuwanjin.broadcastdemo;
 
-import android.*;
-import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import static com.example.xuwanjin.broadcastdemo.BroadcastDemoConstants.LOCAL_BROADCAST;
 import static com.example.xuwanjin.broadcastdemo.BroadcastDemoConstants.ORDERED_BROADCAST;
 import static com.example.xuwanjin.broadcastdemo.BroadcastDemoConstants.PARALLEL_BROADCAST;
 
@@ -15,6 +19,9 @@ public class BroadcastDemoActivity extends AppCompatActivity {
 
     Button sendParallelBroadcast = null;
     Button sendOrderedBroadcast = null;
+    Button sendLocalBroadcast = null;
+    LocalBroadcastManager localBroadcastManager = null;
+    BroadcastReceiver localBroadcastReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,42 @@ public class BroadcastDemoActivity extends AppCompatActivity {
                 sendOrderedBroadcast(sendOrderedBroadcastIntent, null);
             }
         });
+
+        localBroadcastReceiverRegister();
+        sendLocalBroadcast = (Button) findViewById(R.id.local_broadcast);
+        sendLocalBroadcast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent localBroadcastIntent = new Intent();
+                localBroadcastIntent.setAction(LOCAL_BROADCAST);
+                localBroadcastManager.sendBroadcast(localBroadcastIntent);
+                localBroadcastManager.sendBroadcastSync();
+            }
+        });
+
     }
 
+    public void localBroadcastReceiverRegister() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(LOCAL_BROADCAST);
+        localBroadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
+        localBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(LOCAL_BROADCAST)) {
+                    Toast.makeText(context, "Local Broadcast", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        localBroadcastManager.registerReceiver(localBroadcastReceiver , intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(localBroadcastManager != null){
+            localBroadcastManager.unregisterReceiver(localBroadcastReceiver);
+        }
+    }
 }
--- BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/BroadcastDemoConstants.java
+++ BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/BroadcastDemoConstants.java
@@ -10,6 +10,7 @@ public class BroadcastDemoConstants {
 
     public static final String PARALLEL_BROADCAST = "com.example.xuwanjin.broadcastdemo.parallelbroadcast";
     public static final String ORDERED_BROADCAST = "com.example.xuwanjin.broadcastdemo.orderedbroadcast";
+    public static final String LOCAL_BROADCAST = "com.example.xuwanjin.broadcastdemo.localbroadcast";
 
     public static String getBroadcastType(Context context, int resId){
         return context.getResources().getString(resId).trim();
