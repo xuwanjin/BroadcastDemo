0eda3d3fedf5844b0b2f104c679c1ddfbbd7d966
xuwanjin <xuwanjin1991@Gmail.com>
2017-07-01 22:06:53 +0800

    add parallel broadcast
    Signed-off-by: xuwanjin <xuwanjin1991@Gmail.com>

--- BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/BroadcastDemoActivity.java
+++ BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/BroadcastDemoActivity.java
@@ -1,13 +1,26 @@
 package com.example.xuwanjin.broadcastdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BroadcastDemoActivity extends AppCompatActivity {
 
+    Button sendParallelBroadcast = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendParallelBroadcast = (Button) findViewById(R.id.send_parallel_broadcast_intent);
+        sendParallelBroadcast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent parallelBroadcastSendIntent = new Intent();
+                parallelBroadcastSendIntent.setAction("com.example.xuwanjin.broadcastdemo.BroadcastDemoActivity");
+                sendBroadcast(parallelBroadcastSendIntent);
+            }
+        });
     }
 }
---
+++ BroadcastDemo/app/src/main/java/com/example/xuwanjin/broadcastdemo/ParallelBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package com.example.xuwanjin.broadcastdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by xuwanjin on 7/1/17.
+ */
+
+public class ParallelBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("com.example.xuwanjin.broadcastdemo.BroadcastDemoActivity")){
+            Toast.makeText(context, "we just got a ParallelBroadcastReceiver", Toast.LENGTH_LONG).show();
+        }
+    }
+}
